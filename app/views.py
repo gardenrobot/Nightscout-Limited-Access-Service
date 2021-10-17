db6c64949403528b38eb018c0423a4e07f355aad
Sam Erika Clotfelter <samclot@protonmail.com>
2021-10-17 12:58:21 -0400

    move html to template folder

--- app/views.py
+++ app/views.py
@@ -1,20 +1,9 @@
 from app import app, helper
-from flask import request
+from flask import request, render_template
 
 @app.route('/') # TODO put html in template files
 def main(): # TODO password protect this
-    return '''
-<html>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
-  <form action="/share" method="POST">
-    <label for="email">Email to share with:</label><br/>
-    <input name="email" type="email"/><br/>
-    <label for="hours">Hours to share for:</label><br/>
-    <input name="hours" type="number"/><br/>
-    <button type="submit">Share!</button>
-  </form>
-</html>'''
+    return render_template('start.html')
 
 @app.post('/share')
 def share():
@@ -30,9 +19,4 @@ def share():
     # mail subject link to site
 #    helper.send_mail(email, hours, token)
 
-    return '''
-<html>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
-  <div>Sent! A link to the Nightscout app has been sent to their email.</div>
-</html>'''
+    return render_template('share.html')
